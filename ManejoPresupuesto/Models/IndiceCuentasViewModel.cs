1c4bb165e7a256b25ed010733b4ba9aa3cf6e88b
david99cartagena <32483852+david99cartagena@users.noreply.github.com>
2026-03-29 06:48:43 -0500

    modelos de complemento

---
+++ ManejoPresupuesto/Models/IndiceCuentasViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ManejoPresupuesto.Models
+{
+    public class IndiceCuentasViewModel
+    {
+        public string TipoCuenta { get; set; }
+        public IEnumerable<Cuenta> Cuentas { get; set; }
+
+        //suma el balance de cada cuenta
+        public decimal Balance => Cuentas.Sum(x => x.Balance);
+
+        //decimal total = 0;
+        //foreach (var cuenta in Cuentas)
+        //{
+        //    total += cuenta.Balance;
+        //}
+        //return total;
+    }
+}
