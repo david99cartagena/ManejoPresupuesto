b2bed135054673978212e423a97bf1e9058c5b3f
david99cartagena <32483852+david99cartagena@users.noreply.github.com>
2026-03-29 21:17:29 -0500

    servicios de complemento

---
+++ ManejoPresupuesto/Servicios/AutoMapperProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ManejoPresupuesto.Models;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Cuenta, CuentaCreacionViewModel>();
+            // ReverseMap() = “haz el mapeo de ida y vuelta entre las dos clases”.
+            CreateMap<TransaccionActualizacionViewModel, Transaccion>().ReverseMap();
+        }
+    }
+}
---
+++ ManejoPresupuesto/Servicios/RepositorioBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public class RepositorioBase
+    {
+        private readonly string _connectionString;
+        protected RepositorioBase(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        protected SqlConnection CrearConexion()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
