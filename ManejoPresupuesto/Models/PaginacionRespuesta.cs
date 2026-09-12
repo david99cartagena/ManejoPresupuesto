f6d7a3a7a698d6d9ddb9c03cec4b5e66f26489eb
david99cartagena <32483852+david99cartagena@users.noreply.github.com>
2026-09-11 22:33:56 -0500

    paginacion generica reutilizable

---
+++ ManejoPresupuesto/Models/PaginacionRespuesta.cs
@@ -0,0 +1,19 @@
+﻿namespace ManejoPresupuesto.Models
+{
+    public class PaginacionRespuesta
+    {
+        public int Pagina { get; set; } = 1;
+        public int RecordsPorPagina { get; set; } = 10;
+        public int CantidadTotalRecords { get; set; }
+
+        //100 / 5 = 20
+        public int CantidadTotalPaginas => (int)Math.Ceiling((double)CantidadTotalRecords / RecordsPorPagina);
+        public string BaseURL { get; set; }
+    }
+
+    // Uso de generico
+    public class PaginacionRespuesta<T> : PaginacionRespuesta
+    {
+        public IEnumerable<T> Elementos { get; set; }
+    }
+}
