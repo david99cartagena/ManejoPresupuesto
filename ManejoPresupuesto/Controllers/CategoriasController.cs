f3f8c6684b6434ff7ff2b8d54570f17340cf1860
david99cartagena <32483852+david99cartagena@users.noreply.github.com>
2026-09-14 23:17:50 -0500

    paginacion en listado de categorias

--- ManejoPresupuesto/Controllers/CategoriasController.cs
+++ ManejoPresupuesto/Controllers/CategoriasController.cs
@@ -18,16 +18,29 @@ namespace ManejoPresupuesto.Controllers
             this.servicioUsuarios = servicioUsuarios;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(PaginacionViewModel paginacionViewModel)
         {
             var usuarioId = servicioUsuarios.ObtenerUsuarioId();
-            var categorias = await repositorioCategorias.Obtener(usuarioId);
-            return View(categorias);
+            var categorias = await repositorioCategorias.Obtener(usuarioId, paginacionViewModel);
+            var totalCategorias = await repositorioCategorias.Contar(usuarioId);
+
+            var respuestaVM = new PaginacionRespuesta<Categoria>
+            {
+                Elementos = categorias,
+                Pagina = paginacionViewModel.Pagina,
+                RecordsPorPagina = paginacionViewModel.RecordsPorPagina,
+                CantidadTotalRecords = totalCategorias,
+                //Uso de Url.Action() para obtener la URL base de la acción actual
+                BaseURL = Url.Action()
+            };
+
+            return View(respuestaVM);
         }
 
         [HttpGet]
         public IActionResult Crear()
         {
+            //var urlActual = Url.Action();
             return View();
         }
 
