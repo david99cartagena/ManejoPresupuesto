9e39807d8c5ce699b2e06f5d18b2805163e22de4
david99cartagena <32483852+david99cartagena@users.noreply.github.com>
2026-09-16 00:30:22 -0500

    modelos de recuperar password

---
+++ ManejoPresupuesto/Models/RecuperarPasswordViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class RecuperarPasswordViewModel
+    {
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage = "El campo {0} debe ser un correo electrónico válido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string CodigoReseteo { get; set; }
+    }
+}
